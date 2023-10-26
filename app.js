893447929394dc5ee5bd8a86f1b7d86e37bbf4e8
Onkar  Rajendra Charate <77641172+omkaroc27@users.noreply.github.com>
2023-10-26 23:41:40 +0530

    touch

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const path = require('path');
 const app = express();
-const port = 9090;
+const port = 3000;
 mongoose.connect(process.env.MONGO_URI, 
   { useNewUrlParser: true, 
     useUnifiedTopology: true });
